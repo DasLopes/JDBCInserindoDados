afb67b7082b23386ab7d9876a65faab5a80f3c3a
daslopes <daslopes@outlook.com.br>
2024-05-27 16:14:26 -0300

    Inserção com recuperação de Id

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -20,27 +22,27 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES "
-					+ "(?, ?, ?, ?, ?)"
-					);
-			
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
+					"insert into department (Name) values ('D1'), ('D2')",
+					Statement.RETURN_GENERATED_KEYS
+					);			
 			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows Affected: " + rowsAffected);
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);//retorna os valores na colona 1
+					System.out.println("Done! Id = " + id);
+				}
+			}else {
+				System.out.println("No Rows Affected: ");
+			}
+			
+			
 					
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch(ParseException e) {
-			e.printStackTrace();
-		}finally {
+		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
